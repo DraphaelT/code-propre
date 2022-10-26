577d92e8317b79ac6edfdde9cd44f98456c42edd
DraphaelT <115068828+DraphaelT@users.noreply.github.com>
2022-10-26 12:06:35 +0200

    Exercice 1

---
+++ src/main/java/ex1/Calcul.java
@@ -0,0 +1,28 @@
+package ex1;
+
+/**
+ * Classe qui fait different calcul entre a et b 
+ * @author Thomas
+ */
+public class Calcul {
+
+	/**
+	 * Methode qui calcule en additionnent a et b
+	 * @param a int a
+	 * @param b int b
+	 * @return int
+	 */
+	public int addition(int a, int b){
+		return a+b;
+	}
+	
+	/**
+	 * Methode qui calcule en soustrais a et b
+	 * @param a int a
+	 * @param b int b
+	 * @return int
+	 */
+	public int soustraction(int a, int b){
+		return a-b;
+	}
+}
---
+++ src/main/java/ex1/Entreprise.java
@@ -0,0 +1,29 @@
+package ex1;
+
+import java.util.Date;
+
+/**
+ * Classe d'une entreprise qui a pour parametre :
+ * Un nun de siret
+ * un nom
+ * une adresse
+ * et une date
+ * @author Thomas
+ */
+public class Entreprise {
+
+	private int siret;
+	private String nom;
+	private String adresse;
+	private Date dateCreation;
+	
+	public static final int CAPITAL_MAX = 3000000;
+	
+	/**
+	 * Methode qui affiche le status de l'entreprise
+	 */
+	public void AfficherStatut(){
+		
+	}
+	
+}
